18e36b4c136ac904ddba23976611ce45283a11d3
Félix Silva <fmpsilva@outlook.com>
2023-10-04 21:05:16 +0100

    Recipe Edit

--- screens/Food/RecipeEdit.jsx
+++ screens/Food/RecipeEdit.jsx
@@ -1,8 +1,11 @@
 import { View, Text, ScrollView, FlatList, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import { ref_food_ingredients } from "../../firebase.config";
 import { addDoc, deleteDoc, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { color_background_dark, color_button_green, color_button_red, styles_common, styles_text } from "../../styles/styles";
+
 import Input_Text from "../../components/Input_Text";
 import Button_Icon from "../../components/Button_Icon";
 import Label from "../../components/Label";
@@ -17,9 +20,9 @@ export default function RedipeEdit({ navigation, route }) {
   const [label, setLabel]             = useState(recipe ? recipe.label            : "");
   const [servings, setServings]       = useState(servings ? recipe.servings       : 1);
   const [isIngredient, setIsIngredient] = useState(servings ? recipe.isIngredient : false);
-  const [ingredients, setIngredients] = useState(recipe ? recipe.ingredients      : []);
-
+  const [ingredients, setIngredients] = useState(recipe.ingredients ? recipe.ingredients : []);
   const [docs, setDocs] = useState([]);
+  const [removedDocs, setRemovedDocs] = useState([]);
 
 
   
@@ -36,6 +39,11 @@ export default function RedipeEdit({ navigation, route }) {
     const newIngredients = [...ingredients];
     newIngredients.push({label: ingredient.label, data:{ingredientId: ingredient.id, amount: ingredient.unit}});
     setIngredients(newIngredients);
+
+    docs.pop(ingredient);
+    setDocs(docs);
+    removedDocs.push(ingredient);
+    setRemovedDocs(removedDocs);
   }
   const saveRecipe = async () => {
     const data = {
@@ -102,16 +110,16 @@ export default function RedipeEdit({ navigation, route }) {
         </ScrollView>
       </View>
 
-      <View style={{flexDirection: "row", flex: 1}}>
-        <View style={{flex: 1, marginRight: 5, backgroundColor: "#a00"}}>
+      <View style={{flex: 1}}>
+        <View style={styles.container_list}>
           <FlatList
-            data={ingredients}
+            data={docs}
             renderItem={({item}) => { 
               return(
-                <View style={[styles_common.container_front, styles_common.container_list_item]}>
+                <View style={styles.container_item}>
                   <Text style={styles_text.common}>{item.label}</Text>
                   <View style={{flex: 1, justifyContent: "flex-end", flexDirection: "row"}}>
-                    <Button_Icon style={styles.button} icon="delete" onPress={() => {}}/>
+                    <Button_Icon style={styles.button} icon="plus" onPress={() => handleAddIngredientOnPress(item)}/>
                   </View>
                 </View>
               )
@@ -119,17 +127,21 @@ export default function RedipeEdit({ navigation, route }) {
           />
         </View>
 
-        <View style={{width: 1, marginRight: 5, backgroundColor: "#fff"}}/>
+        <View style={{flexDirection: "row", justifyContent: "space-around"}}>
+          <Icon name={"chevron-down"} size={15} color='white'/>
+          <Icon name={"chevron-down"} size={15} color='white'/>
+          <Icon name={"chevron-down"} size={15} color='white'/>
+        </View>
 
-        <View style={{flex: 1, marginLeft: 5}}>
+        <View style={styles.container_list}>
           <FlatList
-            data={docs}
+            data={ingredients}
             renderItem={({item}) => { 
               return(
-                <View style={[styles_common.container_front, styles_common.container_list_item]}>
+                <View style={styles.container_item}>
                   <Text style={styles_text.common}>{item.label}</Text>
                   <View style={{flex: 1, justifyContent: "flex-end", flexDirection: "row"}}>
-                    <Button_Icon style={styles.button} icon="plus" onPress={() => handleAddIngredientOnPress(item)}/>
+                    <Button_Icon style={styles.button} icon="delete" onPress={() => {}}/>
                   </View>
                 </View>
               )
@@ -166,13 +178,24 @@ export default function RedipeEdit({ navigation, route }) {
   );
 }
 
-function Paragraph(props) { return (<View style={{marginLeft: 30}}>{props.children}</View>);}
-function Margin(props) { return (<View style={{margin: props.margin}}/>); }
-
 
 const styles = StyleSheet.create({
   button: {
     marginHorizontal: 2,
     backgroundColor: color_background_dark,
-  }
+  },
+
+  container_item: [
+    styles_common.container_front,
+    styles_common.container_list_item,
+    {marginVertical: 2}
+  ],
+
+  container_list: {
+    flex: 1,
+    marginLeft: 5,
+    borderColor: "#fff",
+    borderTopWidth: 1,
+    borderBottomWidth: 1
+  },
 });
